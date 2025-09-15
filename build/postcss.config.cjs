bc38684071d3e8f5b19816ad435fbbf85ea496b8
Kwikl-Client <kwiklclient@gmail.com>
2025-09-15 16:39:58 +0530

    content updated

---
+++ build/postcss.config.cjs
@@ -0,0 +1,25 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  safelist: {
+    standard: [
+      /^modal/,
+      /^alert/,
+      /^btn/,
+      /^nav/,
+      /^carousel/,
+      /^collapse/,
+      /^fade/,
+      /^show/,
+      /^tooltip/,
+      /^popover/,
+    ],
+  },
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+};
